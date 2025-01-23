5f4156649968cd49c4814e5d56de44b0aac78895
Ahmed <ahmedmahmud1994@hotmail.com>
2025-01-23 09:03:35 +0100

    tinymce editor and tags-input integrated

---
+++ src/components/formelements/TagsFormInput.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import { TagsInput } from "react-tag-input-component";
+
+const TagsFormInput = () => {
+    const [selected, setSelected] = useState([]);
+
+    return (
+        <div>
+            <p className='text-sm mb-1'>press enter (&#8617;) or comma (&#44;) to add new tag</p>
+            <TagsInput
+                value={selected}
+                onChange={setSelected}
+                name="tags"
+                placeHolder="enter tag"
+            />
+            <p className='text-sm mt-2'>{JSON.stringify(selected)}</p>
+        </div>
+    );
+};
+
+export default TagsFormInput;
