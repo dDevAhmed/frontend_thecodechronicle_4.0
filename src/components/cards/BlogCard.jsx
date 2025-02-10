d6f25c3d9b049e7b582a7250a53041dd798f1526
Ahmed <ahmedmahmud1994@hotmail.com>
2025-02-10 02:07:09 +0100

    blogs card - images

--- src/components/cards/BlogCard.jsx
+++ src/components/cards/BlogCard.jsx
@@ -8,31 +8,34 @@ import Button from '../../ui/Button';
 import { useContext } from 'react';
 import AppContext from '../../contexts/AppContext';
 import { PiBookmarkSimpleLight, PiShareFatLight } from 'react-icons/pi';
-import AuthorAvatar from '../../assets/to_remove/author_avatar.jpg'
+import DefaultAvatar from '../../assets/images/default_avatar.png'
 import BlogImage from '../../assets/to_remove/blog_image.webp'
 
 const BlogCard = ({ blog }) => {
-    // author name, avatar
-    // blog title, category, image, date
 
     const { setShowComingSoonModal } = useContext(AppContext);
 
-    // const timeAgo = getTimeAgo(blog.createdAt);
+    const timeAgo = getTimeAgo(blog.createdAt);
+
+    // todo - remove and use timeAgo above
+    const formatDate = (isoString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return new Date(isoString).toLocaleDateString('en-US', options);
+    }
 
     return (
         <Card classNames={'rounded-2xl w-full p-5 bg-white'}>
             <div className={'w-full rounded-2xl flex flex-col justify-between gap-5'}>
                 <div className='flex items-start gap-3'>
-                    <Link to={''}><img src={AuthorAvatar} alt="Author Image" className='h-10 w-10 rounded-full' /></Link>
+                    <Link to={''}><img src={blog.avatar || DefaultAvatar} alt="Author Image" className='h-10 w-10 rounded-full' /></Link>
                     <div className='flex flex-col justify-between'>
                         <Link
                             // to={`/author/${blog.slugTitle}`}
                             to={''}
                             className='text-sm font-bold text-gray-700'>
-                            {/* {capitalizeWords(blog.title)} */}
-                            Author Name
+                            {capitalizeWords(blog.author)}
                         </Link>
-                        <p className='text-sm text-gray-700'>10/02/2025</p>
+                        <p className='text-sm text-gray-700'>{formatDate(blog.createdAt)}</p>
                     </div>
                 </div>
 
@@ -40,27 +43,25 @@ const BlogCard = ({ blog }) => {
                     // to={`/story/${blog.slugTitle}`}
                     to={''}
                     className='font-semibold text-lg w-fit line-clamp-3 text-gray-900'>
-                    {/* {capitalizeWords(blog.title)} */}
-                    Surviving Power Outages: The Developer’s Guide to Staying Charged
+                    {capitalizeWords(blog.title)}
                 </Link>
 
                 {/* <img className="aspect-16/9 rounded-lg object-cover h-10" src={post?.primaryMedia?.url} /> */}
-                <img className="aspect-16/9 rounded-lg object-cover" src={BlogImage} />
+                <img className="aspect-16/9 rounded-lg object-cover bg-white h-44" src={blog.image || BlogImage} />
 
                 <div className='flex items-center justify-between'>
                     <Link
                         // to={`/category/${blog?.category?.name}`}
                         to={''}
                     >
-                        {/* <Pill text={blog?.category?.name} /> */}
-                        <Pill text={'frontend'} />
+                        <Pill text={blog?.category} />
                     </Link>
                     <span className='flex items-center justify-between gap-3'>
                         <Button
                             // classNames={`text-lg border rounded-2xl p-5 flex items-center gap-1 text-gray-600 hover:bg-brand-background-gray ${isBookmarked && 'bg-brand-primary-blue text-white hover:text-brand-primary-white hover:bg-brand-primary-blue'}`}
                             classNames={`group text-lg border rounded-2xl p-5 flex items-center gap-1 text-gray-600 hover:bg-brand-background-gray`}
                             title={'save'}
-                        // onClick={''}
+                            onClick={() => setShowComingSoonModal(true)}
                         >
                             <PiBookmarkSimpleLight className='h-5 w-auto' />
                             {/* {isBookmarked ? 'Bookmarked' : 'Bookmark'} */}
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,17 +1,111 @@
-import { useContext } from 'react'
-import Categories from '../components/Categories'
-// import Card from '../ui/Card';
-// import SearchBar from '../ui/SearchBar';
-// import StoryCardMini from '../components/cards/StoryCardMini';
 import PageTitle from '../ui/PageTitle';
 import AppContext from '../contexts/AppContext';
 import BlogCard from '../components/cards/BlogCard';
 
+// fixme - remove, use api data
+import BlogImage from '../assets/to_remove/blog_image.webp'
+import Baby from '../assets/to_remove/baby.jpg'
+import Fullstack from '../assets/to_remove/fullstack.webp'
+import Docs from '../assets/to_remove/docs.jpg'
+import Laptop from '../assets/to_remove/laptop.png'
+import Native from '../assets/to_remove/native.png'
+import Writing from '../assets/to_remove/writing.jpg'
+import Ethics from '../assets/to_remove/ethics.png'
+import Dark from '../assets/to_remove/dark.webp'
+import Typescript from '../assets/to_remove/typescript.jpg'
+import Hamster from '../assets/to_remove/hamster.webp'
+import Dorcas from '../assets/to_remove/dorcas.jpg'
+import Mercy from '../assets/to_remove/mercy.jpg'
+import Ruqayyat from '../assets/to_remove/ruqayyat.jpg'
+import Onughuh from '../assets/to_remove/onughuh.jpg'
+import Abdulmalik from '../assets/to_remove/abdulmalik.jpg'
+
+const dummyBlogs = [
+    {
+        avatar: Dorcas,
+        author: 'Dorcas',
+        createdAt: '2024-11-07T10:00:00Z',
+        title: 'Read the Docs: The End of ‘src’ Sharing!',
+        image: Docs,
+        category: 'frontend',
+    },
+    {
+        avatar: Abdulmalik,
+        author: 'Abdulmalik',
+        createdAt: '2024-11-12T14:30:00Z',
+        title: 'Mastering TypeScript the CodeLand Way',
+        image: Typescript,
+        category: 'backend',
+    },
+    {
+        avatar: '',
+        author: 'Ahmad Abdulkareen',
+        createdAt: '2024-11-09T09:15:00Z',
+        title: 'From Web to Mobile: CodeLand’s Road to React Native',
+        image: Native,
+        category: 'mobile',
+    },
+    {
+        avatar: '',
+        author: 'Fumbi',
+        createdAt: '2024-11-18T08:45:00Z',
+        title: 'The Art of Documentation: CodeLand’s Writing Mastery',
+        image: Writing,
+        category: 'performance',
+    },
+    {
+        avatar: '',
+        author: 'Fatima',
+        createdAt: '2024-11-22T16:20:00Z',
+        title: 'The Hamster’s Leap into Web3',
+        image: Hamster,
+        category: 'web3',
+    },
+    {
+        avatar: Onughuh,
+        author: 'Godwin Ameh',
+        createdAt: '2024-11-06T11:10:00Z',
+        title: 'Fullstack Odyssey: A CodeLand Explorer’s Guide',
+        image: Fullstack,
+        category: 'community',
+    },
+    {
+        avatar: Mercy,
+        author: 'Mercy',
+        createdAt: '2024-11-14T13:50:00Z',
+        title: 'CodeLand’s Guide to Work Ethics: The Do’s and Don’ts',
+        image: Ethics,
+        category: 'performance',
+    },
+    {
+        avatar: Ruqayyat,
+        author: 'Ruqayyat',
+        createdAt: '2024-11-25T19:05:00Z',
+        title: 'Baby Steps & Bug Fixes: Balancing Code & Cradles',
+        image: Baby,
+        category: 'community',
+    },
+    {
+        avatar: '',
+        author: 'Shamsu',
+        createdAt: '2024-11-10T17:30:00Z',
+        title: 'Laptops Fit for CodeLand Devs: A Buyer’s Guide',
+        image: Laptop,
+        category: 'tool',
+    },
+    {
+        avatar: '',
+        author: 'Divine',
+        createdAt: '2024-11-28T15:40:00Z',
+        title: 'Surviving Power Outages: The Developer’s Guide to Staying Charged',
+        image: Dark,
+        category: 'performance',
+    },
+];
+
 const Blogs = () => {
     // fixme - not a good approach - land at the top by default
     window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    const { initiateExploreSearch } = useContext(AppContext);
 
     return (
         <div className='flex flex-col gap-5 pb-20'>
@@ -24,8 +118,8 @@ const Blogs = () => {
             </div>
 
             <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-                {[1, 2, 3, 4, 5].map((item, index) => (
-                    <BlogCard key={index} />
+                {dummyBlogs.map((blog, index) => (
+                    <BlogCard key={index} blog={blog} />
                 ))}
             </div>
         </div>
