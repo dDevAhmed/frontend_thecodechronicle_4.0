69981a0ea725ae41247347afb6df9167a8650b7a
Ahmed <ahmedmahmud1994@hotmail.com>
2025-02-11 08:29:17 +0100

    add helmet package for open graph documentation

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { useStory } from '../services/StoryService';
+import { capitalizeWords } from '../utils/capitalize'
+import { Helmet } from "react-helmet";
 import StoryHeader from '../components/story/StoryHeader'
 import StoryBody from '../components/story/StoryBody'
 import StoryComments from '../components/story/StoryComments'
 import Spinner from '../ui/Spinner';
-import { useStory } from '../services/StoryService';
 
 const Story = () => {
     // always land at the top of the page
@@ -17,11 +19,26 @@ const Story = () => {
     const { data: post, isPending, isError } = useStory(slugTitle)
 
     if (isPending) {
+        // todo - consider using skeleton
         return <div className='flex items-center justify-center p-10'><Spinner /></div>;
     }
 
+    // todo - if error
+
     return (
         <div className='h-screen relative -mx-[1.125rem] md:px-[1.125rem] md:h-full lg:static lg:flex lg:flex-col lg:gap-5'>
+
+            {/* Add Helmet for dynamic meta tags */}
+            <Helmet>
+                <title>{capitalizeWords(post?.title)}</title>
+                <meta property="og:title" content={capitalizeWords(post?.title)} />
+                {/* //fixme - needs to be generating a description */}
+                <meta property="og:description" content={post?.description || "Explore tech stories from CodeLand—where developers, innovation, and code meet. Stay inspired!"} />
+                {/* //fixme - set default image as the logo if post is video or audio (non image) */}
+                <meta property="og:image" content={post?.primaryMedia.url || "https://thecodechronicle.vercel.app/default-image.jpg"} />
+                <meta property="og:url" content={`https://thecodechronicle.vercel.app/story/${post?.slugTitle}`} />
+            </Helmet>
+
             <div className='fixed h-1/2 w-full md:sticky md:top-[6.75rem] md:h-64 lg:h-72 lg:sticky lg:top-[6.75rem]'>
                 <StoryHeader post={post} />
             </div>
